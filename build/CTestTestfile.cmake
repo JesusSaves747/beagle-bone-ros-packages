a322bb47cc8d2a7a087499f426abb8f4553f38e1
vTechSavyy <psavio5@vt.edu>
2018-03-27 23:04:22 -0400

    Removed build and devel files

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/savio/Documents/ANRA_Projects/beagle-bone-ws/src
-# Build directory: /home/savio/Documents/ANRA_Projects/beagle-bone-ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
-subdirs(usb_cam)
--- build/usb_cam/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/savio/Documents/ANRA_Projects/beagle-bone-ws/src/usb_cam
-# Build directory: /home/savio/Documents/ANRA_Projects/beagle-bone-ws/build/usb_cam
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
